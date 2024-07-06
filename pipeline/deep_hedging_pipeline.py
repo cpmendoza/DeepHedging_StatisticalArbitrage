6208cf65393426b29b185f48b3b13421a1780698
Carlos M <carlos.octavio.perez92@gmail.com>
2024-07-06 13:02:50 -0400

    pipeline

--- pipeline/deep_hedging_pipeline.py
+++ pipeline/deep_hedging_pipeline.py
@@ -13,20 +13,17 @@ sys.path.append(main_folder)
 warnings.filterwarnings("ignore")
 
 from src.utils import *
-from src.features.nig_simulation import *
-from src.features.jivr_simulation import *
+from src.features.features_simulation import *
 from src.models.deep_rl_training import rl_agent
 
 def deel_hedging_pipeline(config_file_simulation,config_file_agent):
 
-    # 1) NIG variables simulation
-    nig_simulation(config_file_simulation)
+    # 1) Market simulator
+    config_file = load_config(os.path.join(main_folder,'cfgs','config_simulation.yml'))
+    market = market_simulator(config_file)
+    market.simulator()
 
-    # 2) JIVR model simulation
-    jivr_model = implied_volatily_surface_vec(config_file_simulation)
-    jivr_model.jivr_simulation()
-
-    # 3) Deep hedging approach
+    # 2) Deep hedging approach
     _ = rl_agent(config_file_simulation,config_file_agent)
 
     return
